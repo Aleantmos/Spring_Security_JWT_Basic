2b226fbc2f43b7d54462896b375a89b7c249948e
Aleatmos <a.moskov95@gmail.com>
2023-05-29 13:02:30 +0300

    must set methor for getting Claims

---
+++ workshop/src/main/java/com/workshop/model/entity/User.java
@@ -0,0 +1,29 @@
+package com.workshop.model.entity;
+
+import com.workshop.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
