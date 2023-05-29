fbb743a42abf604812ed0d4ddd8dcecaf791d35f
Aleatmos <a.moskov95@gmail.com>
2023-05-29 15:26:33 +0300

    Authentication Controller

--- workshop/src/main/java/com/workshop/config/ApplicationConfig.java
+++ workshop/src/main/java/com/workshop/config/ApplicationConfig.java
@@ -5,7 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,5 +23,28 @@ public class ApplicationConfig {
         return new ModelMapper();
     }
 */
+
+    private final MyUserDetailsService myUserDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(myUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
---
+++ workshop/src/main/java/com/workshop/model/dto/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.workshop.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AuthenticationRequest {
+    
+    private String email;
+    private String password;
+    
+}
